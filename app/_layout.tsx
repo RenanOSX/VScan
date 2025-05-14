cebb7778f8888f4c227b4c52863f2310e58c1ada
Darkromano <77361353+RenanOSX@users.noreply.github.com>
2025-05-14 14:03:55 -0300

    Setting up login screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
 import InitialLayout from "@/components/InitialLayout";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import React from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
-        <InitialLayout />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ClerkProvider>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+          <InitialLayout />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ClerkProvider>
   );
 }
