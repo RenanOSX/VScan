d8fb7cbb5e7792efa4879e5a5d116ff2bc451dcc
Darkromano <77361353+RenanOSX@users.noreply.github.com>
2025-05-14 14:42:09 -0300

    Major fixes on login

--- components/InitialLayout.tsx
+++ components/InitialLayout.tsx
@@ -1,10 +1,25 @@
-import React, { Component } from 'react'
-import { Stack } from 'expo-router'
+import { useAuth } from "@clerk/clerk-expo";
+import { Stack, useRouter, useSegments } from "expo-router";
+import { useEffect } from "react";
 
-export class InitialLayout extends Component {
-  render() {
-    return <Stack screenOptions={{headerShown: false}}/>
-  }
+export default function InitialLayout() {
+    const {isLoaded, isSignedIn} = useAuth();
+
+    const segments = useSegments();
+    const router = useRouter();
+
+    useEffect(() => {
+        if(!isLoaded) return;
+
+        const inAuthScreen = segments[0] === "(auth)";
+        
+        // If user not signed in and not in auth screen, redirect to login
+        if(!isSignedIn && !inAuthScreen) router.replace("/(auth)/login");
+        else if(isSignedIn && inAuthScreen) router.replace("/(tabs)/home");
+    }, [isLoaded, isSignedIn, segments]);
+
+    if(!isLoaded) return null;
+
+    // If user is signed in, show the current screen
+    return <Stack screenOptions={{ headerShown: false }} />;
 }
-
-export default InitialLayout
